5f5876118fe5eef5ba498b4473bba3492735e71f
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-21 21:31:17 -0300

    Store created

--- Etapa 1/Loja.java
+++ Etapa 1/Loja.java
@@ -2,4 +2,59 @@ public class Loja {
     private String nome;
     private int quantidadeFuncionarios;
     private double salarioBaseFuncionario;
+
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public Loja(String nome, int quantidadeFuncionarios) {
+        this.nome = nome;
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+        this.salarioBaseFuncionario = -1;
+    }
+
+    //get e set do nome da loja
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    //get e set do numero de funcionarios
+    public int getQuantidadeFuncionarios() {
+        return quantidadeFuncionarios;
+    }
+    public void setQuantidadeFuncionarios(int quantidadeFuncionarios) {
+        this.quantidadeFuncionarios = quantidadeFuncionarios;
+    }
+
+    //get e set do salario
+    public double getSalarioBaseFuncionario() {
+        return salarioBaseFuncionario;
+    }
+    public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
+        this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    public double gastosComSalario() {
+        if(salarioBaseFuncionario == -1) {
+            return -1;
+        }
+        return quantidadeFuncionarios * salarioBaseFuncionario;
+    }
+
+    public char tamanhoDaLoja() {
+        if(quantidadeFuncionarios < 10) {
+            return 'P';
+        }
+        if(quantidadeFuncionarios <= 30) {
+            return 'M';
+        }
+        else {
+            return 'G';
+        }
+    }
 }
