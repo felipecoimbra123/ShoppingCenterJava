5fbe330c2a63fa210dd27db90d3262afd1e4d5a8
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-21 21:46:38 -0300

    adress created

---
+++ Etapa 1/Endereco.java
@@ -0,0 +1,19 @@
+public class Endereco {
+    private String nomeDaRua;
+    private String cidade;
+    private String estado;
+    private String pais;
+    private String cep;
+    private String numero;
+    private String complemento;
+
+    public Endereco(String nomeDaRua, String cidade, String estado, String pais, String cep, String numero, String complemento) {
+        this.nomeDaRua = nomeDaRua;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.pais = pais;
+        this.cep = cep;
+        this.numero = numero;
+        this.complemento = complemento;
+    }
+}
