4d26a5ad9cc4bc8d01bd7f3d501680082efc2271
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-22 21:44:15 -0300

    now bugs fixed

--- Etapa 1/Data.java
+++ Etapa 1/Data.java
@@ -37,7 +37,7 @@ public class Data {
         return dia >= 1 && dia <= diasMes;
     }
 
-    private boolean verificaAnoBissexto() {
+    public boolean verificaAnoBissexto() {
         return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
     }
 
