7c297ba53c479a8e0aafa4c139ba59dd931cdd82
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-22 23:06:28 -0300

    adress and fundation's day in loja.java

--- Structure/Loja.java
+++ Structure/Loja.java
@@ -2,11 +2,15 @@ public class Loja {
     private String nome;
     private int quantidadeFuncionarios;
     private double salarioBaseFuncionario;
+    private Endereco endereco;
+    private Data dataFundacao;
 
-    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario) {
+    public Loja(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao) {
         this.nome = nome;
         this.quantidadeFuncionarios = quantidadeFuncionarios;
         this.salarioBaseFuncionario = salarioBaseFuncionario;
+        this.endereco = endereco;
+        this.dataFundacao = dataFundacao;
     }
 
     public Loja(String nome, int quantidadeFuncionarios) {
@@ -37,6 +41,22 @@ public class Loja {
     }
     public void setSalarioBaseFuncionario(double salarioBaseFuncionario) {
         this.salarioBaseFuncionario = salarioBaseFuncionario;
+    }
+
+    //get e set do endereço
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    //get e set do dia da fundação
+    public Data getDataFundacao() {
+        return dataFundacao;
+    }
+    public void setDataFundacao(Data dataFundacao) {
+        this.dataFundacao = dataFundacao;
     }
 
     public double gastosComSalario() {
