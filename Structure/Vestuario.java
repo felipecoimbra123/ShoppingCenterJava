87dc18de0a52d97da73b3a7f78b6066775f59ff8
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-24 01:28:52 -0300

    class vestuario added

---
+++ Structure/Vestuario.java
@@ -0,0 +1,22 @@
+public class Vestuario extends Loja {
+    private double produtosImportados;
+
+    public Vestuario(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, double produtosImportados) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.produtosImportados = produtosImportados;
+    }
+
+    //get e set dos produtos importados
+    public double getProdutosImportados() {
+        return produtosImportados;
+    }
+
+    public void setProdutosImportados() {
+        this.produtosImportados = produtosImportados;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Produtos importados: " + produtosImportados;
+    }
+}
