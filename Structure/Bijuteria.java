6f6c7f0ec18dc63c0b207cd0314ba7b52741b7aa
felipecoimbra123 <felipe.2007coimbra@gmail.com>
2026-05-24 01:31:58 -0300

    class bijuteria added

---
+++ Structure/Bijuteria.java
@@ -0,0 +1,22 @@
+public class Bijuteria extends Loja {
+    private double metaVendas;
+
+    public Bijuteria(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, double metaVendas) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.metaVendas = metaVendas;
+    }
+
+    //get e set dos produtos importados
+    public double getMetaVendas() {
+        return metaVendas;
+    }
+
+    public void setMetaVendas() {
+        this.metaVendas = metaVendas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Meta de vendas: " + metaVendas;
+    }
+}
